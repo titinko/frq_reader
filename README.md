88068c6b8fcadf13d0498c85a7467bb9337ed2be
Emma Breen <emma.ebreen@gmail.com>
2018-02-08 12:19:40 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+Simple command-line Python utility to print the contents of a binary FREQ0003 (.frq) file.
+
+Usage:
+1. Install Python 2.7.10 or higher.
+1. Navigate to the same directory as frq_reader.py and type:
+> python frq_reader.py
+1. When prompted, enter the fully-qualified pathname of the file you want to parse, or
+just the file name if it's in the same directory as frq_reader.py.
---
+++ frq_reader.py
@@ -0,0 +1,35 @@
+import struct
+
+filename = raw_input("Enter file name:\n")
+
+with open(filename, 'rb') as f:
+	# Header.
+    header_text = f.read(8).decode('utf-8')
+    print header_text
+    
+    # Samples per frq value.  Should always be 256.
+    samples_per_frq = struct.unpack("<i", f.read(4))
+    print "Samples per frq:", samples_per_frq[0]
+    
+    # Average frequency.
+    avg_frq = struct.unpack("<d", f.read(8))
+    print "Average frq:", avg_frq[0]
+    
+    # Empty space.
+    f.read(16)
+    
+    # Number of chunks.
+    num_chunks_bytes = f.read(4)
+    num_chunks = struct.unpack("<i", num_chunks_bytes)
+    print "Number of chunks:", num_chunks[0]
+    
+    print "\nFrequency | Amplitude"
+    for chunk in range(num_chunks[0]):
+    	frequency = struct.unpack("<d", f.read(8))
+    	amplitude = struct.unpack("<d", f.read(8))
+    	print frequency[0], "|", amplitude[0]
+    	
+    print "Successfully parsed frq file!"
+    	
+    
+    
